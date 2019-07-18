52fa451e9e5d669b762dcb4e3d38997b3ba2cd92
The Tushar Srivastav <The Tushar Srivastav@DESKTOP-U07BBKO>
2019-07-18 19:59:58 +0530

    WatsApp Monitor

--- src/whatsapp/whatsapp.java
+++ src/whatsapp/whatsapp.java
@@ -17,32 +17,28 @@ public class whatsapp
 		
 		public static WebDriver driver;
 				@SuppressWarnings("deprecation")
-				public static void main(String[]args) 
+				public static void main(String[]args) throws IOException 
 		{
 					System.setProperty("webdriver.chrome.driver", "C://Users/The Tushar Srivastav/Desktop/chromedriver.exe");  
-					WebDriver driver = new ChromeDriver();  
-			driver=new ChromeDriver();
-			driver.get("https://web.whatsapp.com/");
-			driver.manage().window().maximize();
-			WebDriverWait wait = new WebDriverWait(driver, 20);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='side']/div[1]/div/label")));
-			driver.findElement(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[1]/div/div/div[2]")).click();
-			driver.findElements(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[9]/div/div/div[2]/div[2]/div[2]/span[1]/div/span"));
-			{
-				
-			}
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[17]/div/div/div[2]/div[2]/div[2]/span[1]/div/span")));
-			driver.findElement(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[17]/div/div/div[2]/div[2]/div[2]/span[1]/div/span")).click();
-			WebElement txt = driver.findElement((By.xpath("//*[@id='main']/div[3]/div/div/div[3]/div[23]/div/div/div[1]")));
-			String S=txt.getText();
-			  File DestFile= new File("extractedFilePath");
-			  try {
-				FileUtils.writeStringToFile(DestFile,S );
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			  driver.close();
+					WebDriver driver = new ChromeDriver(); 
+					
+					driver=new ChromeDriver();
+					driver.get("https://web.whatsapp.com/");
+					driver.manage().window().maximize();
+					WebDriverWait wait = new WebDriverWait(driver, 20);
+					wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='side']/div[1]/div/label")));
+					driver.findElement(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[1]/div/div/div[2]")).click();
+					driver.findElements(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[9]/div/div/div[2]/div[2]/div[2]/span[1]/div/span"));
+			
+					
+					wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[17]/div/div/div[2]/div[2]/div[2]/span[1]/div/span")));
+					driver.findElement(By.xpath("//*[@id='pane-side']/div[1]/div/div/div[17]/div/div/div[2]/div[2]/div[2]/span[1]/div/span")).click();
+					WebElement txt = driver.findElement((By.xpath("//*[@id='main']/div[3]/div/div/div[3]/div[23]/div/div/div[1]")));
+					String S=txt.getText();
+					
+					File DestFile= new File("C:/Users/messages");
+					FileUtils.writeStringToFile(DestFile,S);
+					driver.close();
 		
 		}
 
